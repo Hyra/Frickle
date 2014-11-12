cf01e24920732cafb8b33546fe6a13d53b862531
Stef van den Ham <stef@mindthecode.com>
2014-11-12 19:30:37 +0100

    Passport configuration

---
+++ api/config/passport.js
@@ -0,0 +1,61 @@
+var LocalStrategy = require('passport-local').Strategy;
+
+var User = require('../models/user');
+
+module.exports = function(passport) {
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+  passport.use('local-signup', new LocalStrategy({
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true
+  },
+  function(req, email, password, done) {
+    process.nextTick(function() {
+      User.findOne({ 'local.email' :  email }, function(err, user) {
+        if (err)
+          return done(err);
+          if (user) {
+            return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+          } else {
+            var newUser = new User();
+            newUser.local.email = email;
+            newUser.local.password = newUser.generateHash(password);
+            newUser.save(function(err) {
+            if (err)
+              return done(null, false, req.flash('signupMessage', 'Error saving user.'));
+              
+            return done(null, newUser);
+          });
+        }
+      });
+    });
+  }));
+
+  passport.use('local-login', new LocalStrategy({
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true
+  },
+  function(req, email, password, done) {
+    User.findOne({ 'local.email' :  email }, function(err, user) {
+      if (err)
+        return done(err);
+      if (!user)
+        return done(null, false, req.flash('loginMessage', 'No user found.'));
+      if (!user.validPassword(password))
+        return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+
+      return done(null, user);
+    });
+  }));
+
+};
