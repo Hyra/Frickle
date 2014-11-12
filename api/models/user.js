746550276821329286229c27ed37923c7423d042
Stef van den Ham <stef@mindthecode.com>
2014-11-12 19:30:49 +0100

    Example user model with local login and hashed password

--- api/models/user.js
+++ api/models/user.js
@@ -1,11 +1,24 @@
-'use strict';
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
 
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
-
-var UserSchema = new Schema({
-  username: {type: String, index: {unique: true}},
-  email: {type: String, index: {unique: true}}
+// define the schema for our user model
+var UserSchema = mongoose.Schema({
+  local: {
+    email: String,
+    password: String
+  }
 });
 
+// methods ======================
+// generating a hash
+UserSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+UserSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+// create the model for users and expose it to our app
 module.exports = mongoose.model('User', UserSchema);
